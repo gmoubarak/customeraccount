9e77cd055cb31e964a051edeb0993745c6169923
Gerard Moubarak <gmoubarak@ccc.net>
2019-10-29 14:53:56 +0200

    Project Structure Commit

---
+++ src/test/java/exercise/customeraccount/CustomerAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.customeraccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
